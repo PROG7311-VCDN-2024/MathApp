b1e785eaf37964f549a8af23a8a08a1504baff10
lab_services_student <lab_services_student@labVMH8OX>
2024-02-18 11:45:15 +0200

    Add project files.

---
+++ MathApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MathApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
